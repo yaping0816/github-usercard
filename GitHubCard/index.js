9891e004a42adfaebcb3c45fef2763b5e0cc47bf
Yaping Liu <yaping.liu12@outlook.com>
2021-04-15 19:57:48 -0700

    finished step 1-3

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,9 +1,17 @@
+import axios from 'axios'
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
-
+axios.get('https://api.github.com/users/yaping0816')
+  .then(res =>{
+    // debugger
+    console.log(res.data.name)
+  })
+  .catch(err =>{
+    debugger
+  })
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
